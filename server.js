31df12045a1b12e9de24306c0dde7aee4d5e6593
Anamika Rajput <anamikarajput0832@gmail.com>
2023-04-19 22:50:49 +0530

    include .js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./db");
+const connectDB = require("./db.js");
 const router = require("./routes.js");
 dotenv.config();
 
