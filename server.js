5e7c3a25a223751ba7b56395e60cbe41c66beda8
Anamika Rajput <anamikarajput0832@gmail.com>
2023-04-20 10:54:10 +0530

    changes on landing page

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ import cors  from "cors";
 import dotenv  from "dotenv";
 import connectDB  from "./db.js";
 import router  from "./routes.js";
+
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from 'cookie-parser';
+
+import morgan from 'morgan';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -12,6 +22,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './portfolio/build')));
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(cookieParser());
 app.use("/api/", router);
 
 const PORT = process.env.PORT || 5000;
